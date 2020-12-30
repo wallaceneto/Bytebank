b3471f8e7aa7a856b818dd1f9c34fced5998c4ec
Wallace Neto <wallace.neto@outlook.com>
2020-12-30 14:52:27 -0300

    Implementacao das aulas do modulo 1

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Column(
+      children: <Widget>[
+        Text(
+          'Deliver features faster',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Expanded(
+            child: FittedBox(
+          fit: BoxFit.contain,
+          child: const FlutterLogo(),
+        ))
+      ],
+    ));
