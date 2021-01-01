e91f544e1d73093bed859aa4e2be63d415e4e9b1
Wallace Neto <wallace.neto@outlook.com>
2021-01-01 19:25:29 -0300

    Adicionando tela de deposito

--- lib/screens/dashboard/dasboard.dart
+++ lib/screens/dashboard/dasboard.dart
@@ -1,9 +1,10 @@
 import 'package:bytebank2/models/saldo.dart';
 import 'package:bytebank2/screens/dashboard/saldo.dart';
+import 'package:bytebank2/screens/deposito/formulario.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-final String _dashboardTitle = 'Bytebank';
+const String _dashboardTitle = 'Bytebank';
 
 class Dashboard extends StatelessWidget {
   @override
@@ -18,24 +19,12 @@ class Dashboard extends StatelessWidget {
             alignment: Alignment.topCenter,
             child: SaldoCard(),
           ),
-          Consumer<Saldo>(
-            builder: (context, saldo, child) {
-              return RaisedButton(
-                child: Text('Adiciona'),
-                onPressed: () {
-                  saldo.adiciona(10);
-                },
-              );
-            },
-          ),
-          Consumer<Saldo>(
-            builder: (context, saldo, child) {
-              return RaisedButton(
-                child: Text('Subtrai'),
-                onPressed: () {
-                  saldo.subtrai(10);
-                },
-              );
+          RaisedButton(
+            child: Text('Receber depósito'),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return FormularioDeposito();
+              }));
             },
           ),
         ],
